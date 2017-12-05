c0e7d03fd71e2f5d776ab67e411cda55b2375e82
AhmedMoawad <ahmed.aboelenin@tajawal.com>
2017-12-05 02:38:12 +0200

    empty structure

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,19 +1,1 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 
-const Letter = ({isVisible, value}) => (
-    <span className="App--sentence--letter">
-        {isVisible ? value : '_'}
-    </span>
-)
-
-Letter.defaultProps = {
-    isVisible: false
-};
-
-Letter.propTypes = {
-    isVisible: PropTypes.bool.isRequired,
-    value: PropTypes.string.isRequired
-};
-
-export default Letter;
